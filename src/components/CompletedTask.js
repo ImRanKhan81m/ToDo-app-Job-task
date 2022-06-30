55f97ebfc751bc1e0c676d45c4f27bdd2b54886c
Imran Hossen <freelancerimran81@gmail.com>
2022-07-01 02:02:11 +0600

    get complete task done

--- src/components/CompletedTask.js
+++ src/components/CompletedTask.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const CompletedTask = () => {
+    const [completeTasks, setCompleteTasks] = useState([]);
+    const [isReload, setIsReload] = useState(false);
+
+    useEffect(() => {
+        fetch("http://localhost:5000/complete-task")
+            .then((res) => res.json())
+            .then((data) => setCompleteTasks(data));
+    }, [isReload]);
+
+
     return (
-        <div>
-            <h1>This is Completed task page.</h1>
+        <div className='mid-content'>
+            Total Complete: {completeTasks.length}
         </div>
     );
 };
